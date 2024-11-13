5329bcd90b3088f5bbd5688b5ded52cae6bbdb4f
Vladislav Pochernin <vspochernin@gmail.com>
2024-11-13 22:33:19 +0300

    lab-3.11: implement char methods

--- lab-3.11/src/main/java/ru/vspochernin/MyArrays.java
+++ lab-3.11/src/main/java/ru/vspochernin/MyArrays.java
@@ -3,11 +3,13 @@ package ru.vspochernin;
 public class MyArrays {
 
     // Поиск по всему массиву.
+    // 1.
     public static int binarySearch(byte[] a, byte key) {
         return binarySearch(a, 0, a.length, key);
     }
 
     // Поиск по массиву на полуинтервале [fromIndex, toIndex).
+    // 2.
     public static int binarySearch(byte[] a, int fromIndex, int toIndex, byte key) {
         int low = fromIndex;
         int high = toIndex - 1;
@@ -25,5 +27,30 @@ public class MyArrays {
             }
         }
         return -(low + 1);  // Ключ не найден.
+    }
+
+    // 3.
+    public static int binarySearch(char[] a, char key) {
+        return binarySearch(a, 0, a.length, key);
+    }
+
+    // 4.
+    public static int binarySearch(char[] a, int fromIndex, int toIndex, char key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            char midVal = a[mid];
+
+            if (midVal < key) {
+                low = mid + 1;
+            } else if (midVal > key) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -(low + 1);
     }
 }
--- lab-3.11/src/test/java/ru/vspochernin/MyArraysTest.java
+++ lab-3.11/src/test/java/ru/vspochernin/MyArraysTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyArraysTest {
 
-    public static final int NUM_TESTS = 1000000; // Количество тестов на каждый тип.
+    public static final int NUM_TESTS = 100_000; // Количество тестов на каждый тип.
     public static final int MAX_ARRAY_SIZE = 100; // Максимальный размер массива на каждую итерацию теста.
 
     public Random random = new Random();
@@ -23,7 +23,7 @@ class MyArraysTest {
             int len = array.length;
 
             for (int j = 0; j < len; j++) {
-                array[j] = (byte) random.nextInt(Byte.MIN_VALUE * Byte.MIN_VALUE); // Генерация случайного байта.
+                array[j] = (byte) random.nextInt(256); // Генерация случайного байта.
             }
 
             // Предварительная сортировка массива.
@@ -32,13 +32,14 @@ class MyArraysTest {
             // Берем ключ для поиска либо из массива, либо случайный.
             byte key = random.nextInt(10) >= 5 && len > 0
                     ? array[random.nextInt(len)]
-                    : (byte) random.nextInt(Byte.MIN_VALUE * Byte.MIN_VALUE);
+                    : (byte) random.nextInt(256);
 
-            // Проверка всего массива
+            // Проверка всего массива.
             assertEquals(
                     Arrays.binarySearch(array, key),
                     MyArrays.binarySearch(array, key),
-                    "Некорректный ответ для массива: " + Arrays.toString(array) + ", key: " + key);
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
 
             int fromIndex = len > 1
                     ? random.nextInt(len / 2)
@@ -54,8 +55,49 @@ class MyArraysTest {
                     "Некорректный ответ для массива: " + Arrays.toString(array) +
                             ", key: " + key +
                             ", fromIndex: " + fromIndex +
-                            ", toIndex: " + toIndex
-            );
+                            ", toIndex: " + toIndex);
+        }
+    }
+
+
+    @Test
+    public void testCharBinarySearchRandomArrays() {
+        for (int i = 0; i < NUM_TESTS; i++) {
+            random = new Random();
+
+            char[] array = new char[random.nextInt(256)];
+            int len = array.length;
+
+            for (int j = 0; j < len; j++) {
+                array[j] = (char) random.nextInt(65536);
+            }
+
+            Arrays.sort(array);
+
+            char key = random.nextInt(10) >= 5 && len > 0
+                    ? array[random.nextInt(len)]
+                    : (char) random.nextInt(65536);
+
+            assertEquals(
+                    Arrays.binarySearch(array, key),
+                    MyArrays.binarySearch(array, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key);
+
+            int fromIndex = len > 1
+                    ? random.nextInt(len / 2)
+                    : 0;
+            int toIndex = len > 1
+                    ? fromIndex + random.nextInt(len / 2)
+                    : len == 1 ? 1 : 0;
+
+            assertEquals(
+                    Arrays.binarySearch(array, fromIndex, toIndex, key),
+                    MyArrays.binarySearch(array, fromIndex, toIndex, key),
+                    "Некорректный ответ для массива: " + Arrays.toString(array) +
+                            ", key: " + key +
+                            ", fromIndex: " + fromIndex +
+                            ", toIndex: " + toIndex);
         }
     }
 }
